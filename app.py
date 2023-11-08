1fdca64560dbbfed39fd9a941db45bd259903694
rachida0001 <rachida.guedouari.ro@gmail.com>
2023-11-08 12:04:24 +0100

    add the html code

--- app.py
+++ app.py
@@ -12,14 +12,18 @@ model = joblib.load('model.pkl')
 def index():
     return render_template('index.html')
 
-@app.route("/predict", methods = ["POST"])
+@app.route('/predict', methods=["POST"])
 def predict():
-    float_features = [float(x) for x in request.form.values()]
-    features = [np.array(float_features)]
-    prediction = model.predict([features])
-    if prediction==1:
-        txt = ""
-    return render_template('index.html', prediction_text ="the breast cancer is {}".format(prediction))
+    if request.method == 'POST':
+        float_features = [float(x) for x in request.form.values()]
+        features = [np.array(float_features)]
+        # Preprocess the data as needed
+        prediction = model.predict(features)
+        if prediction==1:
+            txt = "benign"
+        else:
+            txt = "malignant"
+        return render_template('index.html', prediction_text ="the breast cancer is {}".format(txt))
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug=True)
